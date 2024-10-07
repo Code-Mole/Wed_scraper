0c5d1cfedfe845ace3dc0bd72a89d205ba994d1a
Asangabia Mole <moleasangabia23@gmail.com>
2024-10-07 16:45:17 +0000

    web scraper displaying data from the Database(MYSQL)

--- Client/web_scraper/src/App.js
+++ Client/web_scraper/src/App.js
@@ -1,18 +1,12 @@
-import react, { useState,useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import react, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [url, setUrl] = useState('');
-  const [selectors, setSelectors] = useState({
-    leadItem: '.profile-card', // Default selectors (adjust these for your specific sites)
-    name: '.name',
-    email: '.email',
-    phone: '.phone',
-    company: '.company',
-    website: '.website'
-  });
+  const [url, setUrl] = useState("");
+  const [selectors, setSelectors] = useState({});
   const [scrapedData, setScrapedData] = useState([]);
+  const [fetchData, setFetchData] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false); // Track loading state
 
@@ -22,149 +16,156 @@ function App() {
     setIsLoading(true); // Set loading state to true
 
     try {
-      const response = await axios.post('http://localhost:3000/scrape', { url, selectors });
+      const response = await axios.post("http://localhost:8500/scrape", {
+        url,
+        selectors,
+      });
       setScrapedData(response.data);
-      console.log('Data scraped and stored successfully!');
+      console.log("Data scraped and stored successfully!");
     } catch (error) {
-      console.error('Error scraping:', error);
-      setErrorMessage('Failed to scrape data. Please check your URL and selectors.');
+      console.error("Error scraping:", error);
+      setErrorMessage(
+        "Failed to scrape data. Please check your URL and selectors."
+      );
     } finally {
       setIsLoading(false); // Set loading state to false after scraping
     }
   };
 
+  // fech data from the backend
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8500/leads");
+        setFetchData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
-      <h1 className="mb-4">Web Scraping Tool</h1>
+        <h1 className="title">Web Scraping Tool</h1>
 
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="url">Website URL:</label>
-          <input
-            type="text"
-            id="url"
-            className="form-control"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            required
-          />
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="url">Website URL:</label>
+            <br></br>
+            <input
+              type="text"
+              id="url"
+              className="form-control"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              required
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="leadItem">Lead Item Selector (CSS selector):</label>
-          <input
-            type="text"
-            id="leadItem"
-            className="form-control"
-            value={selectors.leadItem}
-            onChange={(e) => setSelectors({ ...selectors, leadItem: e.target.value })}
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="leadItem">Lead Item Selector (CSS selector):</label>
+            <br></br>
+            <input
+              type="text"
+              id="leadItem"
+              className="form-control"
+              value={selectors.leadItem}
+              onChange={(e) =>
+                setSelectors({ ...selectors, leadItem: e.target.value })
+              }
+              required
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="name">Name Selector:</label>
-          <input
-            type="text"
-            id="name"
-            className="form-control"
-            value={selectors.name}
-            onChange={(e) => setSelectors({ ...selectors, name: e.target.value })}
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="name">Name Selector:</label>
+            <br></br>
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              value={selectors.name}
+              onChange={(e) =>
+                setSelectors({ ...selectors, name: e.target.value })
+              }
+              required
+            />
+          </div>
 
-        <div className="form-group">
-        <label htmlFor="email">Email Selector:</label>
-          <input
-            type="text"
-            id="email"
-            className="form-control"
-            value={selectors.email}
-            onChange={(e) => setSelectors({ ...selectors, email: e.target.value })}
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="email">Email Selector:</label>
+            <br></br>
+            <input
+              type="text"
+              id="email"
+              className="form-control"
+              value={selectors.email}
+              onChange={(e) =>
+                setSelectors({ ...selectors, email: e.target.value })
+              }
+              required
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="phone">Phone Selector:</label>
-          <input
-            type="text"
-            id="phone"
-            className="form-control"
-            value={selectors.phone}
-            onChange={(e) => setSelectors({ ...selectors, phone: e.target.value })}
-            required
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="phone">Phone Selector:</label>
+            <br></br>
+            <input
+              type="text"
+              id="phone"
+              className="form-control"
+              value={selectors.phone}
+              onChange={(e) =>
+                setSelectors({ ...selectors, phone: e.target.value })
+              }
+              required
+            />
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="company">Company Selector:</label>
-          <input
-            type="text"
-            id="company"
-            className="form-control"
-            value={selectors.company}
-            onChange={(e) => setSelectors({ ...selectors, company: e.target.value })}
-            required
-          />
-        </div>
+          <button type="submit" className="btn " disabled={isLoading}>
+            {isLoading ? "Scraping..." : "Scrape"}
+          </button>
+        </form>
 
-        <div className="form-group">
-          <label htmlFor="website">Website Selector:</label>
-          <input
-            type="text"
-            id="website"
-            className="form-control"
-            value={selectors.website}
-            onChange={(e) => setSelectors({ ...selectors, website: e.target.value })}
-            required
-          />
-        </div>
+        {errorMessage && (
+          <div className="alert" role="alert">
+            {errorMessage}
+          </div>
+        )}
 
-        <button type="submit" className="btn btn-primary" disabled={isLoading}>
-          {isLoading ? 'Scraping...' : 'Scrape'}
-        </button>
-      </form>
+        {scrapedData.message && (
+          <div className="alert" role="alert">
+            {scrapedData.message}
+          </div>
+        )}
 
-      {errorMessage && (
-        <div className="alert alert-danger mt-3" role="alert">
-          {errorMessage}
-        </div>
-      )}
+        <h2 className="table-title ">Scraped Leads:</h2>
 
-      {scrapedData.message && (
-        <div className="alert alert-success mt-3" role="alert">
-          {scrapedData.message}
-        </div>
-      )}
-
-      <h2 className="mt-4">Scraped Leads:</h2>
-
-      <table className="table table-striped mt-3">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Company</th>
-            <th>Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scrapedData.length > 0 && scrapedData.map((lead, index) => (
-            <tr key={index}>
-              <td>{lead.name}</td>
-              <td>{lead.email}</td>
-              <td>{lead.phone}</td>
-              <td>{lead.company}</td>
-              <td>{lead.website}</td>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>IDs</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {fetchData.length > 0 &&
+              fetchData.map((lead, index) => (
+                <tr key={index}>
+                  <td>{lead.ID}</td>
+                  <td>{lead.name}</td>
+                  <td>{lead.email}</td>
+                  <td>{lead.phone}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./dbConnection.js";
-import {scrapeWebsite} from "./scrape.js";
+import { scrapeWebsite } from "./scrape.js";
 
 const Server = express();
 const PORT = 8500;
@@ -12,26 +12,17 @@ Server.use(express.json());
 
 // methods
 
-
-
 // POST route
 Server.post("/scrape", async (req, res) => {
   const { url, selectors } = req.body;
 
   try {
-    const scrapedData = await scrape.scrapeWebsite(url, selectors);
+    const scrapedData = await scrapeWebsite(url, selectors);
 
     // Insert data into database
-    const sql =
-      "INSERT INTO leads (name, email, phone, company, website) VALUES (?, ?, ?, ?, ?)";
+    const sql = "INSERT INTO scrape_data (name, email, phone) VALUES (?, ?, ?)";
     for (const lead of scrapedData) {
-      const values = [
-        lead.name,
-        lead.email,
-        lead.phone,
-        lead.company,
-        lead.website,
-      ];
+      const values = [lead.name, lead.email, lead.phone];
       db.query(sql, values, (err, result) => {
         if (err) {
           console.error("Error inserting lead:", err);
@@ -46,6 +37,19 @@ Server.post("/scrape", async (req, res) => {
     res.status(500).json({ error: "Failed to scrape website" });
   }
 });
+
+// Get route
+
+Server.get("/leads", (req, res) => {
+  const sql = "SELECT * FROM scrape_data";
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error("Error fetching leads:", err);
+      res.status(500).json({ error: "Failed to fetch leads" });
+    }
+    res.json(result);
+  });
+})
 
 Server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
